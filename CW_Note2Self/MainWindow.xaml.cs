b71d8d47819693a1357e84bbd8f8f958222871f0
moony-bilcherry <erchinskaya.nataliya@gmail.com>
2021-05-18 21:56:51 +0300

    начинаю бд

--- CW_Note2Self/MainWindow.xaml.cs
+++ CW_Note2Self/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,56 +11,46 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Linq.Expressions;
-
 
 namespace CW_Note2Self
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        public List<ICalendarEvent> _events;
-        public List<ICalendarEvent> Events
-        {
-            get { return _events; }
-            set
-            {
-                if (_events != value)
-                {
-                    _events = value;
-                    OnPropertyChanged(() => Events);
-
-                    // redraw days with events when Events property changes
-                    Calendar.DrawDays();
-                }
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-
-
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void closeApp(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
-            // set date of first example event to +- middle of month
-            DateTime startDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 15);
+        private void minimiseApp(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
-            // add example events
-            Events = new List<ICalendarEvent>();
-            Events.Add(new MyCustomEvent() { DateFrom = DateTime.Now, DateTo = DateTime.Now.AddDays(2), Label = "Event 1" });
-            Events.Add(new MyCustomEvent() { DateFrom = startDate.AddDays(2), DateTo = startDate.AddDays(5), Label = "Overlapping event 2" });
-            //Events.Add(new MyCustomEvent() { DateFrom = startDate.AddDays(4), DateTo = startDate.AddDays(6), Label = "Overlapping event 3" });
-            //Events.Add(new MyCustomEvent() { DateFrom = startDate.AddDays(7), DateTo = startDate.AddDays(8), Label = "Event 4" });
-
-            // draw days with events calendar
-            Calendar.DrawDays();
-
-            // subscribe to double cliked event
-            Calendar.CalendarEventDoubleClickedEvent += Calendar_CalendarEventDoubleClickedEvent;
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
         }
     }
 }
