f65b6c058142f6bd252cc4709e981e9753ecbecb
moony-bilcherry <erchinskaya.nataliya@gmail.com>
2021-05-18 12:20:52 +0300

    ффффффффф

--- CW_Note2Self/Register.xaml.cs
+++ CW_Note2Self/Register.xaml.cs
@@ -10,21 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CW_Note2Self
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Register.xaml
     /// </summary>
-    public partial class Welcome : Window
+    public partial class Register : Window
     {
-        public Welcome()
+        public Register()
         {
             InitializeComponent();
         }
-
         private void closeApp(object sender, MouseButtonEventArgs e)
         {
             try
@@ -55,6 +53,4 @@ namespace CW_Note2Self
                 DragMove();
         }
     }
-
-    
 }
