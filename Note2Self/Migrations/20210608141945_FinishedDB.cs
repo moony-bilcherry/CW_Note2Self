4bdb18bf4b5b9c98df0dddb151bb458d6d07a7b9
moony-bilcherry <erchinskaya.nataliya@gmail.com>
2021-06-08 18:35:10 +0300

    БУДЕТ ТАК

---
+++ Note2Self/Migrations/20210608141945_FinishedDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Note2Self.Migrations
+{
+    public partial class FinishedDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
