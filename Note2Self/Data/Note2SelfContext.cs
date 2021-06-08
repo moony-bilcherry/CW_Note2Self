cf6c589c665d79e6204eb52044e740237b3c2a7b
moony-bilcherry <erchinskaya.nataliya@gmail.com>
2021-06-08 18:51:28 +0300

    работаем!

--- Note2Self/Data/Note2SelfContext.cs
+++ Note2Self/Data/Note2SelfContext.cs
@@ -12,6 +12,7 @@ namespace Note2Self.Models
         public DbSet<Users> Users { get; set; }
         public DbSet<Notes> Notes { get; set; }
         public DbSet<Goals> Goals { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //modelBuilder.Entity<Notes>()
@@ -25,8 +26,8 @@ namespace Note2Self.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
         {
-            //dbContextOptionsBuilder.UseSqlServer(@"Data Source=MOONY-SHTO;Initial Catalog=Note2Self;Integrated Security=True");
-            dbContextOptionsBuilder.UseSqlServer(@"Server=tcp:note2selfserver.database.windows.net,1433;Initial Catalog=app;Persist Security Info=False;User ID=moony;Password=Pamag1te;");
+            dbContextOptionsBuilder.UseSqlServer(@"Data Source=MOONY-SHTO;Initial Catalog=Note2Self;Integrated Security=True");
+            //dbContextOptionsBuilder.UseSqlServer(@"Server=tcp:note2selfserver.database.windows.net,1433;Initial Catalog=app;Persist Security Info=False;User ID=moony;Password=Pamag1te;");
         }
     }
 }
